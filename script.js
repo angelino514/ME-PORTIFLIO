5c55188ec08e94f246c8936b6eaec785f32d390d
Angelino Eurico <angelinoeurico574@gmail.com>
2025-11-01 17:23:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.querySelector('.button-add-menu').addEventListener('click' , ()=>{
-    document.querySelector('.menu-navbar').classList.toggle('activo-navbar')
+let button = document.querySelector('.button-add-menu')
+
+button.addEventListener('click' , ()=>{
+   document.querySelector('.menu-navbar').classList.toggle('activo-navbar')
+    button.classList.toggle('activo-line')
 })
