7f54e1cb610bb913c9ccab9805c25ef4879ed3cb
Angelino Eurico <angelinoeurico574@gmail.com>
2025-11-05 19:51:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,39 @@ button.addEventListener('click' , ()=>{
    document.querySelector('.menu-navbar').classList.toggle('activo-navbar')
     button.classList.toggle('activo-line')
 })
+
+
+let lastOne = document.querySelectorAll('.last-one')
+
+let myObserve = new IntersectionObserver(entries =>{
+
+    entries.forEach(entriy =>{
+        if(entriy.isIntersecting) {
+            entriy.target.classList.add("activo")
+        }
+
+        else {
+            entriy.target.classList.remove("activo")
+        }
+    })
+})
+
+lastOne.forEach(lasts =>{
+    myObserve.observe(lasts)
+})
+
+let hidden = document.querySelector('.hidden')
+
+let hiddenObserver = new IntersectionObserver(h_entries =>{
+    h_entries.forEach(h_entry =>{
+     if(h_entry.isIntersecting){
+        h_entry.target.classList.add('h_activo')
+     }
+
+     else {
+        h_entry.target.classList.remove('h_activo')
+     }
+    })
+})
+
+hiddenObserver.observe(hidden)
